f1da4204a410b9c7163e2778af3219d9ab508c8a
Andreas Klinger <a.klinger@kkf-solutions.de>
2021-07-18 17:59:00 +0200

    Changed title

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test 5
-          Edit <code>src/App.tsx</code> and save to reload.
+          CI/CD React Test
+          React test for continuous integration and continuous delivery/deployment.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
